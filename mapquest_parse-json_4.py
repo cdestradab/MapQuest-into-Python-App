fed29631aa77d96042fe39634d504604d8b55c7a
cdestradab <cdestradab@gmail.com>
2022-06-25 08:04:47 +0000

    fixed quit funciontality

--- mapquest_parse-json_4.py
+++ mapquest_parse-json_4.py
@@ -8,12 +8,12 @@ key = "pAXPZMssSGEm4bvGQaSUOTPMjBeWh6W5";
 
 while True:
     orig = input("Starting Location: ");
+    dest = input("Destination: ");
     if orig == "quit" or orig == "q":
         break;
     if dest == "quit" or dest == "q":
         break;
 
-    dest = input("Destination: ");
     url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest});
     print("URL: " + (url));
     json_data = requests.get(url).json();
