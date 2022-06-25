02a197182479d49760567dca0b8c15e5e02651e4
cdestradab <cdestradab@gmail.com>
2022-06-25 06:10:27 +0000

    importacion de librerias y creacion de variables

---
+++ mapquest_parse-json_1.py
@@ -0,0 +1,8 @@
+import urllib.parse
+import requests
+
+main_api = "http://www.mapquestapi.com/directions/v2/route?";
+orig = "Washington, D.C.";
+dest = "Baltimore, Md";
+key = "pAXPZMssSGEm4bvGQaSUOTPMjBeWh6W5";
+
