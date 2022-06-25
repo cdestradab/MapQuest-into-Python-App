8e48596733003de324bed85cf8581263c0a64080
cdestradab <cdestradab@gmail.com>
2022-06-25 07:15:33 +0000

    get request

--- mapquest_parse-json_1.py
+++ mapquest_parse-json_1.py
@@ -7,3 +7,6 @@ dest = "Baltimore, Md";
 key = "pAXPZMssSGEm4bvGQaSUOTPMjBeWh6W5";
 
 url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest});
+
+json_data = requests.get(url).json();
+print(json_data);
