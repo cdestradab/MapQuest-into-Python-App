4e0f84e2edf38d20fffb0be7f5be2de04edff768
cdestradab <cdestradab@gmail.com>
2022-06-25 07:57:50 +0000

    add input functionality test1

--- mapquest_parse-json_2.py
+++ mapquest_parse-json_2.py
@@ -15,5 +15,3 @@ json_status = json_data["info"]["statuscode"];
 
 if json_status == 0:
     print("API Status: " + str(json_status) + " = A successful route call.\n");
-
-
---
+++ mapquest_parse-json_3.py
@@ -0,0 +1,17 @@
+import urllib.parse
+import requests
+
+main_api = "http://www.mapquestapi.com/directions/v2/route?";
+#orig = "Bogotá, Colombia";
+#dest = "Arauca, Colombia";
+key = "pAXPZMssSGEm4bvGQaSUOTPMjBeWh6W5";
+
+while True:
+    orig = input("Starting Location: ");
+    dest = input("Destination: ");
+    url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest});
+    print("URL: " + (url));
+    json_data = requests.get(url).json();
+    json_status = json_data["info"]["statuscode"];
+    if json_status == 0:
+        print("API Status: " + str(json_status) + " = A successful route call.\n");
